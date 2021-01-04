63bf7cac4620b3cd8d88e95d4cdf789d4defe368
01320Nabi <35794170+01320Nabi@users.noreply.github.com>
2021-01-04 15:21:11 +0900

    fixed syntax errors, used additional libraries

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from dimigo import document, exit
+from dimigo import document, cube, office, exit
 from datetime import date
+import os
 
 seoryu = document.get('자퇴')
 seoryu.set(document.fields.NAME, os.getenv('name'))
 seoryu.set(document.fields.DATE, date.today())
 seoryu.set(document.fields.REASON, "탈디미 너무달아")
 
-seoryu.print(device.getPrintableDevice())
+jatweseo = cube.print(seoryu)
 
-if accepted:
-  print("자퇴성공! 탈디미!")
-  exit()
+if office.request(jatweseo) == office.response.accepted:
+    print("자퇴성공! 탈디미!")
+    exit()
 else:
-  print("자퇴실패")
-  life.exit()
+    print("자퇴실패")
+    life.exit()
